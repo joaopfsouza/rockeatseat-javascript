1b06a34bd62bd85eaf7ccf993b3bb7715dd74ec7
João Paulo Ferreira de Souza <joao.ferreiradesouza@hotmail.com>
2019-09-04 18:44:37 -0300

    Promises

---
+++ ajax/main.js
@@ -0,0 +1,47 @@
+var apiUrl = 'https://api.github.com/users/joaopfsouza';
+
+function AjaxReques() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://api.github.com/users/joaopfsouza');
+    xhr.send(null);
+    xhr.onreadystatechange = function () {
+        console.log(this.readyState);
+        if (xhr.readyState === 4) {
+            console.log(JSON.parse(xhr.responseText));
+        }
+    };
+}
+
+var MyPromise = function () {
+    return new Promise(function (resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'https://api.github.com/users/joaopfsouza');
+        xhr.send(null);
+
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    resolve(JSON.parse(xhr.responseText));
+                } else {
+                    reject('Erro requisição');
+                }
+            }
+        }
+    });
+};
+
+// MyPromise()
+// .then(function(response){
+//     console.log(response);
+// })
+// .catch(function(error){
+//     console.warn(error)
+// });
+
+axios.get(apiUrl)
+.then(function(response){
+    console.log(response);
+})
+.catch(function(error){
+    console.warn(error)
+});
